89103a5d4faea931893acdc44d7caef2b6b773dd
shridhar kalagi <shridharkalagi@gmail.com>
2019-03-12 16:21:52 +0530

    Add suport for multiple browsers

--- src/test/java/com/Utils/BaseClass.java
+++ src/test/java/com/Utils/BaseClass.java
@@ -3,10 +3,13 @@ package com.Utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import java.util.concurrent.TimeUnit;
+
+import static io.github.bonigarcia.wdm.DriverManagerType.PHANTOMJS;
 
 public class BaseClass {
     public WebDriver driver;
@@ -15,16 +18,27 @@ public class BaseClass {
 
     @BeforeClass
     public void setUp() {
-        getBrowser();
+        getFireFoxBrowser();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.get(baseUrl);
     }
 
-    private void getBrowser() {
+    private void getChromeBrowser() {
+        System.out.println("Setting up Chrome Browser");
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
     }
 
+    private void getFireFoxBrowser() {
+        System.out.println("Setting up FireFox Browser");
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+    }
+
+    private void getPhantomJsBrowser() {
+        System.out.println("Setting up PhantomJs Browser");
+        WebDriverManager.getInstance(PHANTOMJS).setup();
+    }
 
     @AfterClass
     public void teardown() {
